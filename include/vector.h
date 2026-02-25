75d3168a44cc74772a913fe8ac1b25bf57e55db6
Rithvik <84326832+rithvikr9@users.noreply.github.com>
2026-02-25 17:29:13 +0530

    Use struct instead of class for Vec2 & implement operator overloading

--- include/vector.h
+++ include/vector.h
@@ -1,25 +1,27 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include <cmath>
 #include <iostream>
 
-class Vec2 {
-	private:
-		float x, y;
-	
-	public:
-		Vec2(int x, int y) {
-			this->x = x;
-			this->y = y;
-		};
+struct Vec2 {
+	float x, y;
 
-		int getX();
-		int getY();
-		float magnitude();
-		Vec2 add(Vec2 other);
-		Vec2 scale(float k);
-		float dot(Vec2 other);
-		void print();
+	Vec2 operator+(const Vec2 &other) { return {x + other.x, y + other.y}; }
+	Vec2 operator-(const Vec2 &other) { return {x - other.x, y - other.y}; }
+	Vec2 operator*(const float scalar) { return {x * scalar, y * scalar}; }
+	Vec2 operator/(const float scalar) { return {x / scalar, y / scalar}; }
+
+	float magnitude() { return sqrt(x * x + y * y); }
+
+	Vec2 normalize() {
+		float m = magnitude();
+		return (m > 0) ? Vec2{x / m, y / m} : Vec2{0, 0};
+	}
+
+	void print() {
+		std::cout << "(" << x << ", " << y << ")" << std::endl;
+	}
 };
 
 #endif
